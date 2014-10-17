c2ce12e92a73407e0813407d67d4359d4b10fe36
Cameron Brown <cameron@quentin.sx>
2014-10-18 04:42:54 +1100

    More comments & formatting

--- Interpreter.cs
+++ Interpreter.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 
 namespace BFInterpret {
+
     class Interpreter {
 
         // Amount of memory needed, I try to aim for as small as possible
@@ -15,30 +16,32 @@ namespace BFInterpret {
         public Interpreter(string script) {
             memory = new byte[MEM_SIZE];
             doActions(script);
+
             // Spitting out memory information
             Console.WriteLine("Dump:");
             for(int i = 0; i <= highestUsedMemory; i++) {
+                // Sticking 0s on the front for neatness
                 string num = memory[i] < 10 ? "00" + memory[i].ToString() : memory[i] < 100 ? "0" + memory[i].ToString() : memory[i].ToString();
                 Console.Write("{0} ", num);
             }
             Console.Write("\n");
-            for(int i = 0; i < pointer; i++)
+            for(int i = 0; i < pointer; i++) // Putting spaces before the pointer indicator
                 Console.Write("    ");
             Console.Write(" ^\nOutput:\n{0}", output);
         }
 
-        int looppointer = -1;
+        int looppointer = -1; // Used to show where loops start
         private void doActions(string ac) {
             for(int i = 0; i < ac.Length; i++) {
-                Console.WriteLine("At index {0}", i);
+                Console.WriteLine("At index {0}", i); // To keep track in big scripts
                 int p = performSingleAction(ac.ToCharArray()[i]);
-                if(p == 1)
+                if(p == 1) // Start of a loop
                     looppointer = i;
-                if(looppointer != -1 && p == 2) {
-                    if(memory[pointer] != 0)
+                if(looppointer != -1 && p == 2) { // Checking that we're in a loop (looppointer) and at the end of the loop
+                    if(memory[pointer] != 0) // Loops only end if the resulting address is at 0
                         i = looppointer;
                     else
-                        looppointer = -1;
+                        looppointer = -1; // Set to -1 meaning we're out of the loop
                 }
                 Console.Clear();
             }
@@ -46,6 +49,7 @@ namespace BFInterpret {
 
         // Performs the action of a single character
         // Ignores non-brainfuck characters too!
+        // Returns 1 if start of loop, 2 if end of loop and 0 otherwise
         private int performSingleAction(char c) {
             switch(c) {
             case '<':
@@ -57,6 +61,7 @@ namespace BFInterpret {
                 pointer++;
                 if(pointer >= MEM_SIZE)
                     pointer = 0;
+                // This is used to display only relevant bytes, not *all* of them
                 highestUsedMemory = Math.Max(highestUsedMemory, pointer);
                 break;
             case '+':
@@ -100,4 +105,5 @@ namespace BFInterpret {
         }
 
     }
+
 }
